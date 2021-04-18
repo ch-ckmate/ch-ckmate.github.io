e543d2d2561967d956f68f8d7a1f81fd6f68a87d
Alessandro Pigato <checkmaster0@gmail.com>
2021-04-18 14:44:08 +0200

    Update particles.js

--- particles.js
+++ particles.js
@@ -27,7 +27,7 @@ var pJS = function(tag_id, params){
         }
       },
       color: {
-        value: '#fff'
+        value: '12CE09'
       },
       shape: {
         type: 'circle',
@@ -36,7 +36,7 @@ var pJS = function(tag_id, params){
           color: '#ff0000'
         },
         polygon: {
-          nb_sides: 5
+          nb_sides: 2
         },
         image: {
           src: '',
@@ -67,7 +67,7 @@ var pJS = function(tag_id, params){
       line_linked: {
         enable: true,
         distance: 100,
-        color: '#fff',
+        color: '12CE09',
         opacity: 1,
         width: 1
       },
